3c997de5cec5bf5f110d5f9e2e05ac012aafd166
Lord Rakesh <89836389+lordrakesh@users.noreply.github.com>
2021-09-28 00:26:48 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '919002370263', 'JJ-Oreki', m)
+  this.sendContact(m.chat, '918473956301', 'Lord Rakesh', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
